34d306e8c0ec43f78cde83a5aa722876bfcacea4
Thomas Kelly <tkelly@scottlogic.co.uk>
2015-02-01 19:22:16 +0000

    Tweak the Game Over screen
    Removes all the excessive colouring, and only lists the winning factions

--- Werewolf Moderator/Werewolf Moderator/PlayControllers/GameOverViewController.m
+++ Werewolf Moderator/Werewolf Moderator/PlayControllers/GameOverViewController.m
@@ -25,13 +25,6 @@
     // Do any additional setup after loading the view.
     self.winningFactions = [SingleGame game].factionsWhichWon;
     
-    self.factionColours = @[[UIColor blackColor],
-                            [UIColor colorWithRed:254.0/255 green:200.0/255 blue:130.0/255 alpha:1.0],
-                            [UIColor colorWithRed:166.0/255 green:206.0/255 blue:227.0/255 alpha:1.0],
-                            [UIColor colorWithRed:255.0/255 green:237.0/255 blue:111.0/255 alpha:1.0],
-                            [UIColor colorWithRed:250.0/255 green:159.0/255 blue:181.0/255 alpha:1.0],
-                            [UIColor colorWithRed:035.0/255 green:139.0/255 blue:069.0/255 alpha:1.0]];
-    
     self.factionTable.delegate = self;
     self.factionWinTable.delegate = self;
     self.factionTable.dataSource = self;
@@ -47,7 +40,7 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 6;
+    return self.winningFactions.count;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -58,20 +51,11 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"faction"];
     }
     
-    if (tableView == self.factionTable)
-    {
-        cell.textLabel.text = [self factionName:indexPath.row];
-        cell.backgroundColor = [self.factionColours objectAtIndex:indexPath.row];
-        cell.textLabel.textColor = (indexPath.row == WolvesFaction) ? [UIColor whiteColor] : [UIColor blackColor];
-    }
-    else
-    {
-        BOOL factionWon = [self.winningFactions containsObject:@(indexPath.row)];
-        cell.textLabel.text = @"";
-        cell.backgroundColor = (factionWon)
-            ? [UIColor colorWithRed:51.0/255 green:160.0/255 blue:44.0/255 alpha:1.0]
-            : [UIColor colorWithRed:227.0/255 green:26.0/255 blue:28.0/255 alpha:1.0];
-    }
+    cell.textLabel.textColor = [UIColor whiteColor];
+    cell.backgroundColor = [UIColor clearColor];
+
+    NSInteger factionIndex = [[[self.winningFactions allObjects] objectAtIndex:indexPath.row] integerValue];
+    cell.textLabel.text = [self factionName:factionIndex];
     
     return cell;
 }
